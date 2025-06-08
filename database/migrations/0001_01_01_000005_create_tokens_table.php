8f9f42b850905b23378c5f9ca1625369a9593e61
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-07 23:14:25 -0300

    Melhorias

--- database/migrations/0001_01_01_000005_create_tokens_table.php
+++ database/migrations/0001_01_01_000005_create_tokens_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->default('token');
-            $table->string('password');
             $table->string('token', Token::getLengthCodeByColumn('token'))->unique();
             $table->timestamps();
         });
