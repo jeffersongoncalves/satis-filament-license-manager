e93dc119e2a9c3ff2afd23214faac4e10499114d
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-07 23:14:11 -0300

    Melhorias

--- app/Providers/Auth/EloquentTokenProvider.php
+++ app/Providers/Auth/EloquentTokenProvider.php
@@ -129,10 +129,6 @@ class EloquentTokenProvider implements UserProvider
      */
     public function retrieveByCredentials(#[\SensitiveParameter] array $credentials)
     {
-        if (empty($credentials)) {
-            return;
-        }
-
         // First we will add each credential element to the query as a where clause.
         // Then we can execute the query and, if we found a user, return it in a
         // Eloquent User "model" that will be utilized by the Guard instances.
