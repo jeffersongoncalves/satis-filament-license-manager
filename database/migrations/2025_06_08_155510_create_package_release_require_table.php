6162e2a757e5c4eca17de6d469fc8186d6a856f4
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-08 17:35:34 -0300

    Melhorias

--- database/migrations/2025_06_08_155510_create_package_release_require_table.php
+++ database/migrations/2025_06_08_155510_create_package_release_require_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('package_release_requires', function (Blueprint $table) {
+        Schema::create('package_release_require', function (Blueprint $table) {
             $table->id();
             $table->foreignId('package_id')->constrained()->cascadeOnDelete();
             $table->foreignId('package_release_id')->constrained()->cascadeOnDelete();
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('package_release_requires');
+        Schema::dropIfExists('package_release_require');
     }
 };
