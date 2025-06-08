b6166d1e314f9fb0ba200a80ddee1103ff9c0a46
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-07 23:00:34 -0300

    Melhorias

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\Composer\PackagesController;
 use App\Http\Controllers\Composer\IncludeController;
 use App\Http\Controllers\Composer\PackagesV2Controller;
 use App\Http\Middleware\EnsureUserHasLicense;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['auth.basic:token', EnsureUserHasLicense::class]], function (): void {
+Route::group(['middleware' => [AuthenticateWithBasicAuth::using('token'), EnsureUserHasLicense::class]], function (): void {
     Route::get('packages.json', PackagesController::class)->withoutMiddleware(EnsureUserHasLicense::class);
     Route::get('include/{include}.json', IncludeController::class)->withoutMiddleware(EnsureUserHasLicense::class);
     Route::get('p2/{vendor}/{package}.json', PackagesV2Controller::class);
