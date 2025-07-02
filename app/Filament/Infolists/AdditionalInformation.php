f2a5cc8d8b6f8908a942673915581555068c2df9
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-02 05:37:44 -0300

    refactor(filament): add return type and simplify date mapping in AdditionalInformation
    Specify `Section` as the return type for the `make` method. Remove unnecessary white-space for consistency and improve code readability.

--- app/Filament/Infolists/AdditionalInformation.php
+++ app/Filament/Infolists/AdditionalInformation.php
@@ -7,9 +7,9 @@ use Filament\Infolists\Components\TextEntry;
 
 class AdditionalInformation
 {
-    public static function make(array $dates = ['created_at', 'updated_at'])
+    public static function make(array $dates = ['created_at', 'updated_at']): Section
     {
-        $dates = collect($dates)->map(fn ($date) => TextEntry::make($date)->dateTime())->toArray();
+        $dates = collect($dates)->map(fn($date) => TextEntry::make($date)->dateTime())->toArray();
 
         return Section::make(__('ADDITIONAL INFORMATION'))
             ->description(__('Information on the date of registration and date of modification.'))
