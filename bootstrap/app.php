014b53f5a98de5cc065ded32e30aa9e481327789
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-02 16:12:33 -0300

    refactor(routes): move route definitions to bootstrap configuration
    Simplify the application structure by relocating composer-related routes from `routes/web.php` to `bootstrap/app.php`. Update `.gitignore` to include new environment-related JSON files.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\Composer\ArchivesController;
+use App\Http\Controllers\Composer\IncludeController;
+use App\Http\Controllers\Composer\PackagesController;
+use App\Http\Controllers\Composer\PackagesV2Controller;
+use App\Http\Middleware\EnsureUserHasLicense;
 use App\Support\FaviconSupport;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +21,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         then: function () {
             FaviconSupport::routes();
+
+            Route::group(['middleware' => [AuthenticateWithBasicAuth::using('token'), EnsureUserHasLicense::class]], function (): void {
+                Route::get('packages.json', PackagesController::class)->withoutMiddleware(EnsureUserHasLicense::class);
+                Route::get('include/{include}.json', IncludeController::class)->withoutMiddleware(EnsureUserHasLicense::class);
+                Route::get('p2/{vendor}/{package}.json', PackagesV2Controller::class);
+                Route::get('archives/{vendor}/{package}/{file}', ArchivesController::class)->where('file', '.*\.zip');
+            });
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,1 @@
 <?php
-
-use App\Http\Controllers\Composer\ArchivesController;
-use App\Http\Controllers\Composer\IncludeController;
-use App\Http\Controllers\Composer\PackagesController;
-use App\Http\Controllers\Composer\PackagesV2Controller;
-use App\Http\Middleware\EnsureUserHasLicense;
-use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['middleware' => [AuthenticateWithBasicAuth::using('token'), EnsureUserHasLicense::class]], function (): void {
-    Route::get('packages.json', PackagesController::class)->withoutMiddleware(EnsureUserHasLicense::class);
-    Route::get('include/{include}.json', IncludeController::class)->withoutMiddleware(EnsureUserHasLicense::class);
-    Route::get('p2/{vendor}/{package}.json', PackagesV2Controller::class);
-    Route::get('archives/{vendor}/{package}/{file}', ArchivesController::class)->where('file', '.*\.zip');
-});
