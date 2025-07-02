9529e589498ec6946c9e38527956a0c07f98182d
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-02 06:30:53 -0300

    refactor(operations): replace job with one-time operation for package updates
    Remove `ProcessSatisByPathAndRepositoryUrl` job and replace it with a reusable, asynchronous one-time operation to update all packages. Simplify logic by reusing `ProcessPackageDependency`.

---
+++ operations/2025_07_02_062909_update_packages.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Jobs\ProcessPackageDependency;
+use App\Models\Package;
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
+
+return new class extends OneTimeOperation
+{
+    /**
+     * Determine if the operation is being processed asynchronously.
+     */
+    protected bool $async = true;
+
+    /**
+     * The queue that the job will be dispatched to.
+     */
+    protected string $queue = 'default';
+
+    /**
+     * A tag name, that this operation can be filtered by.
+     */
+    protected ?string $tag = null;
+
+    /**
+     * Process the operation.
+     */
+    public function process(): void
+    {
+        $packages = Package::query()->get();
+
+        foreach ($packages as $package) {
+            ProcessPackageDependency::dispatch($package);
+        }
+    }
+};
