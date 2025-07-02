f9df529ffa474fda550f02203416a4cc0fe97d0d
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-02 06:16:21 -0300

    feat(operations): add one-time operation to update dependency types
    Introduce a one-time operation to update dependency types based on name matching. Dependencies containing '/' are updated via `Packagist::getDependencyType`, while others are set to `DependencyType::Public`. Set the operation to run asynchronously with default queue settings.

---
+++ operations/2025_07_02_061325_update_dependencies.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Enums\DependencyType;
+use App\Models\Dependency;
+use App\Models\Packagist;
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
+
+return new class extends OneTimeOperation {
+    /**
+     * Determine if the operation is being processed asynchronously.
+     */
+    protected bool $async = true;
+
+    /**
+     * The queue that the job will be dispatched to.
+     */
+    protected string $queue = 'default';
+
+    /**
+     * A tag name, that this operation can be filtered by.
+     */
+    protected ?string $tag = null;
+
+    /**
+     * Process the operation.
+     */
+    public function process(): void
+    {
+        $dependencies = Dependency::query()->get();
+
+        foreach ($dependencies as $dependency) {
+            if (str($dependency->name)->contains('/')) {
+                $dependency->update(['type' => Packagist::getDependencyType($dependency->name)]);
+            } else {
+                $dependency->update(['type' => DependencyType::Public]);
+            }
+        }
+    }
+};
