f475dcc2b9e6de30632f5751c41613e9abd49d4c
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-02 09:46:02 -0300

    fix(archives): handle missing archive file with 404 response
    Add a File existence check in ArchivesController to return a 404 response if the requested archive file is missing. Improve error handling for better user feedback.

--- app/Http/Controllers/Composer/ArchivesController.php
+++ app/Http/Controllers/Composer/ArchivesController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Composer;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ArchivesController
 {
     public function __invoke(Request $request, string $vendor, string $package, string $file)
     {
-        return response()->file(storage_path("app/private/satis/{$request->user('token')->id}/archives/{$vendor}/{$package}/{$file}"));
+        if (! File::exists($path = storage_path("app/private/satis/{$request->user('token')->id}/archives/{$vendor}/{$package}/{$file}"))) {
+            return response()->noContent(404);
+        }
+        return response()->file($path);
     }
 }
