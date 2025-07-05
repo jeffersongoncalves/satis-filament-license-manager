bfde7a7c22b2d52575cd3a9370e11fbc5fd0e382
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-04 21:15:52 -0300

    fix(migrations): prevent adding duplicate versions column to dependencies table
    Add a condition to check if the `versions` column exists before altering the `dependencies` table, ensuring the migration does not attempt to add the column multiple times.

--- database/migrations/2025_06_13_192124_add_versions_to_dependencies_table.php
+++ database/migrations/2025_06_13_192124_add_versions_to_dependencies_table.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        if (Schema::hasColumn('dependencies', 'versions')) {
+            return;
+        }
         Schema::table('dependencies', function (Blueprint $table) {
             $table->json('versions')->nullable();
         });
